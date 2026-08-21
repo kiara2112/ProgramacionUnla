0625394a0ae40851b37fe85a806ebd32bcb3029a
kiara2112 <kiarafalsa123@gmail.com>
2026-08-20 21:40:32 -0300

    Update code8.c

--- code8.c
+++ code8.c
@@ -1,3 +1,6 @@
+//Siempre se hace la carga del sueldo, pero si el sueldo que ingresamos supera 3000 pesos se mostrará por pantalla el mensaje 
+//"Esta persona debe abonar impuestos", en caso que la persona cobre 3000 o menos no aparece nada por pantalla.
+
 #include<stdio.h>
 #include<conio.h>
 
