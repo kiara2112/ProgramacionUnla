7d03ff55f57c8a360f4346e79cb9891c12896bb2
kiara2112 <kiarafalsa123@gmail.com>
2026-08-31 19:33:04 -0300

    Update code101.c

--- code101.c
+++ code101.c
@@ -1,23 +1,23 @@
 //Confeccionar un programa que defina dos funciones, una que permita cargar un vector de 5 elementos enteros y otra que muestre un vector de 5 elementos enteros.
-//En la función main definir una variable de tipo vector y seguidamente llamar a las dos funciones.
+//En la funciÃ³n main definir una variable de tipo vector y seguidamente llamar a las dos funciones.
 
 #include<stdio.h>
 #include<conio.h>
-
-void cargar(int vec[5])
+//a diferencia de int, char, float, etc. Vector si se edita en un void, se cambia en vector original de main
+void cargar(int vec[5]) //el parametro vec no recibe una copia del vector, si no que guarda la referencia
 {
     int x;
-    for(x=0;x<5;x++)
+    for(x=0;x<5;x++)//asi no supera los 5 puestos
     {
-        printf("Ingrese elemento:");
-        scanf("%i",&vec[x]);
+        printf("Ingrese elemento:"); 
+        scanf("%i",&vec[x]); //carga cada elemento en la posicion de X
     }
 }
-
+//void no puede retornar valores, pero si puede mostrar
 void imprimir(int vec[5])
 {
     int x;
-    printf("Contenido completo del vector:");
+    printf("Contenido completo del vector:"); //luego del ingreso, se va a mostrar los valores
     for(x=0;x<5;x++)
     {
         printf("%i ",vec[x]);
