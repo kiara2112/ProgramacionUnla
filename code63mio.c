52ed397aa2751e0013739b1ced6470df5511bcbe
kiara2112 <kiarafalsa123@gmail.com>
2026-09-01 00:35:17 -0300

    Update code63mio.c

--- code63mio.c
+++ code63mio.c
@@ -1,9 +1,36 @@
-//Cargar un vector de 10 elementos y verificar posteriormente si el mismo est· ordenado de menor a mayor
+//Cargar un vector de 10 elementos y verificar posteriormente si el mismo est√° ordenado de menor a mayor
 
 #include<stdio.h>
 #include<conio.h>
 
 int main()
 {
-    float
+    float vector[10];
+    int f;
+    int orden=0;
+    for (f=0; f<10; f++)
+    {
+        printf("ingresar valor: ");
+        scanf("%i", &vector[f]);
+    }
+
+    for(f=0; f<9; f++)
+    {
+        if (vector[f+1]<vector[f])//ve la posicion que se encuentra f y la proxima, y las compara si la primera ingresada es mayor a la segundo (el +1 no es la cantidad, si no la posicion de f, la proxima)
+        {
+            orden=1;
+            break; //funciona para salir de la estructura repetitiva, para que no siga comparando si no es necesario (se puede utilizar en cualquier repetidor)
+        }
+    }
+    if (orden==0)//si el numero sigue siendo 0 (como lo nombramos fuera del if), ejecuta que si esta ordenado en orden, si no, va a al else y dice que no esta ordenado sin dar mucho mas detalles
+    {
+        printf("Esta ordenado de menor a mayor");
+    }
+    else
+    {
+        printf("No esta ordenado de menor a mayor");
+    }
+    getch();
+    return 0;
+
 }
