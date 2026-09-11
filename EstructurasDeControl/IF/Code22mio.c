2a330e3539e993503f85977f8abab7ffff4a3382
kiara2112 <kiarafalsa123@gmail.com>
2026-09-10 23:50:23 -0300

    8 - Condiciones compuestas con operadores lógicos
    Se ingresan por teclado tres números, si todos los valores ingresados son menores a 10, imprimir en pantalla la leyenda "Todos los números son menores a diez".

--- EstructurasDeControl/IF/Code22mio.c
+++ EstructurasDeControl/IF/Code22mio.c
@@ -1,5 +1,5 @@
-//Se ingresan por teclado tres números, si todos los valores ingresados son menores a 10,
-//imprimir en pantalla la leyenda "Todos los números son menores a diez".
+//Se ingresan por teclado tres nÃºmeros, si todos los valores ingresados son menores a 10,
+//imprimir en pantalla la leyenda "Todos los nÃºmeros son menores a diez".
 
 #include<stdio.h>
 #include<conio.h>
