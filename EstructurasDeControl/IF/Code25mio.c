1a7a08e762f3e5454fe78625750a83ef567b86bd
kiara2112 <kiarafalsa123@gmail.com>
2026-09-10 23:52:29 -0300

    8 - Condiciones compuestas con operadores lógicos
    De un operario se conoce su sueldo y los años de antigüedad. Se pide confeccionar un programa que lea los datos de entrada e informe:
    a) Si el sueldo es inferior a 500 y su antigüedad es igual o superior a 10 años, otorgarle un aumento del 20 %, mostrar el sueldo a pagar.
    b)Si el sueldo es inferior a 500 pero su antigüedad es menor a 10 años, otorgarle un aumento de 5 %.
    c) Si el sueldo es mayor o igual a 500 mostrar el sueldo en pantalla sin cambios.

--- EstructurasDeControl/IF/Code25mio.c
+++ EstructurasDeControl/IF/Code25mio.c
@@ -1,6 +1,6 @@
-//De un operario se conoce su sueldo y los años de antigüedad. Se pide confeccionar un programa que lea los datos de entrada e informe:
-//a) Si el sueldo es inferior a 500 y su antigüedad es igual o superior a 10 años, otorgarle un aumento del 20 %, mostrar el sueldo a pagar.
-//b)Si el sueldo es inferior a 500 pero su antigüedad es menor a 10 años, otorgarle un aumento de 5 %.
+//De un operario se conoce su sueldo y los aÃ±os de antigÃ¼edad. Se pide confeccionar un programa que lea los datos de entrada e informe:
+//a) Si el sueldo es inferior a 500 y su antigÃ¼edad es igual o superior a 10 aÃ±os, otorgarle un aumento del 20 %, mostrar el sueldo a pagar.
+//b)Si el sueldo es inferior a 500 pero su antigÃ¼edad es menor a 10 aÃ±os, otorgarle un aumento de 5 %.
 //c) Si el sueldo es mayor o igual a 500 mostrar el sueldo en pantalla sin cambios.
 
 #include<stdio.h>
