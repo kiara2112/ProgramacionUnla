2129f9692363f30542fe2ac4034e31bbe0632a55
kiara2112 <kiarafalsa123@gmail.com>
2026-09-11 00:03:26 -0300

    9 - Estructura repetitiva while
    Realizar un programa que permita cargar dos listas de 15 valores cada una. Informar con un mensaje cual de las dos listas tiene un valor acumulado mayor (mensajes "Lista 1 mayor", "Lista 2 mayor", "Listas iguales")
    Tener en cuenta que puede haber dos o más estructuras repetitivas en un algoritmo.

--- EstructurasDeControl/WHILE/Code36mio.c
+++ EstructurasDeControl/WHILE/Code36mio.c
@@ -1,6 +1,6 @@
 //Realizar un programa que permita cargar dos listas de 15 valores cada una. Informar con un mensaje
 //cual de las dos listas tiene un valor acumulado mayor (mensajes "Lista 1 mayor", "Lista 2 mayor", "Listas iguales")
-//Tener en cuenta que puede haber dos o m·s estructuras repetitivas en un algoritmo.
+//Tener en cuenta que puede haber dos o m√°s estructuras repetitivas en un algoritmo.
 
 #include<stdio.h>
 #include<conio.h>
