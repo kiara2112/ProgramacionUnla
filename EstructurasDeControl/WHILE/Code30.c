a204578754fbc895c334256f7500e99b3dab32aa
kiara2112 <kiarafalsa123@gmail.com>
2026-09-10 23:57:52 -0300

    9 - Estructura repetitiva while
    Una planta que fabrica perfiles de hierro posee un lote de n piezas.
    Confeccionar un programa que pida ingresar por teclado la cantidad de piezas a procesar y luego ingrese la longitud de cada perfil; sabiendo que la pieza cuya longitud esté comprendida en el rango de 1.20 y 1.30 son aptas. Imprimir por pantalla la cantidad de piezas aptas que hay en el lote.

--- EstructurasDeControl/WHILE/Code30.c
+++ EstructurasDeControl/WHILE/Code30.c
@@ -1,6 +1,6 @@
 //Una planta que fabrica perfiles de hierro posee un lote de n piezas.Confeccionar un programa que pida ingresar
 //por teclado la cantidad de piezas a procesar y luego ingrese la longitud de cada perfil; sabiendo que la pieza cuya longitud
-//esté comprendida en el rango de 1.20 y 1.30 son aptas. Imprimir por pantalla la cantidad de piezas aptas que hay en el lote.
+//estÃ© comprendida en el rango de 1.20 y 1.30 son aptas. Imprimir por pantalla la cantidad de piezas aptas que hay en el lote.
 
 
 #include<stdio.h>
