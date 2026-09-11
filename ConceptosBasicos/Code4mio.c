3e8933651b11c590b161ba7bf41a86486587860d
kiara2112 <kiarafalsa123@gmail.com>
2026-09-10 23:25:52 -0300

    Estructura de programación secuencial
    Realizar la carga del lado de un cuadrado, mostrar por pantalla el perímetro del mismo (El perímetro de un cuadrado se calcula multiplicando el valor del lado por cuatro)

--- ConceptosBasicos/Code4mio.c
+++ ConceptosBasicos/Code4mio.c
@@ -1,5 +1,5 @@
-//Realizar la carga del lado de un cuadrado, mostrar por pantalla el perímetro del mismo
-//(El perímetro de un cuadrado se calcula multiplicando el valor del lado por cuatro)
+//Realizar la carga del lado de un cuadrado, mostrar por pantalla el perÃ­metro del mismo
+//(El perÃ­metro de un cuadrado se calcula multiplicando el valor del lado por cuatro)
 
 #include<stdio.h>
 #include<conio.h>
