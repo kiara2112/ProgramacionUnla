2472549856731da2458ed14c82e76dcbc78ccc4b
kiara2112 <kiarafalsa123@gmail.com>
2026-09-10 23:44:02 -0300

    7 - Estructuras condicionales anidadas
    Se ingresa por teclado un valor entero, mostrar una leyenda que indique si el número es positivo, negativo o nulo (es decir cero)

--- EstructurasDeControl/IF/Code15mio.c
+++ EstructurasDeControl/IF/Code15mio.c
@@ -1,4 +1,4 @@
-//Se ingresa por teclado un valor entero, mostrar una leyenda que indique si el número es positivo, negativo o nulo (es decir cero)
+//Se ingresa por teclado un valor entero, mostrar una leyenda que indique si el nÃºmero es positivo, negativo o nulo (es decir cero)
 
 #include<stdio.h>
 #include<conio.h>
