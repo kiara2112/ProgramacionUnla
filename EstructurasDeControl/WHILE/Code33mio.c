223c551d3c5fe1f31476fdcb8df1bd2d4b4ae966
kiara2112 <kiarafalsa123@gmail.com>
2026-09-11 00:01:12 -0300

    9 - Estructura repetitiva while
    En una empresa trabajan n empleados cuyos sueldos oscilan entre $100 y $500, realizar un programa que lea los sueldos que cobra cada empleado e informe cuántos empleados cobran entre $100 y $300 y cuántos cobran más de $300. Además el programa deberá informar el importe que gasta la empresa en sueldos al personal

--- EstructurasDeControl/WHILE/Code33mio.c
+++ EstructurasDeControl/WHILE/Code33mio.c
@@ -1,5 +1,5 @@
 //En una empresa trabajan n empleados cuyos sueldos oscilan entre $100 y $500, realizar un programa que lea los sueldos que cobra cada empleado
-//e informe cu·ntos empleados cobran entre $100 y $300 y cu·ntos cobran m·s de $300. Adem·s el programa deber· informar el importe que gasta la empresa en sueldos al personal.
+//e informe cu√°ntos empleados cobran entre $100 y $300 y cu√°ntos cobran m√°s de $300. Adem√°s el programa deber√° informar el importe que gasta la empresa en sueldos al personal.
 
 #include<stdio.h>
 #include<conio.h>
