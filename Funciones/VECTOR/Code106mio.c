6067367afc8d093df74e22e7be5038891b9777da
kiara2112 <kiarafalsa123@gmail.com>
2026-09-14 23:06:54 -0300

    19 - Funciones con parámetros de tipo vector
    Una empresa tiene dos turnos (mañana y tarde) en los que trabajan 8 empleados (4 por la mañana y 4 por la tarde)
    Confeccionar un programa que permita almacenar los sueldos de los empleados agrupados por turno.
    Imprimir los gastos en sueldos de cada turno.
    Se deben implementar las siguientes funciones:

--- Funciones/VECTOR/Code106mio.c
+++ Funciones/VECTOR/Code106mio.c
@@ -1,4 +1,4 @@
-//Una empresa tiene dos turnos (mañana y tarde) en los que trabajan 8 empleados (4 por la mañana y 4 por la tarde)
+//Una empresa tiene dos turnos (maÃ±ana y tarde) en los que trabajan 8 empleados (4 por la maÃ±ana y 4 por la tarde)
 //Confeccionar un programa que permita almacenar los sueldos de los empleados agrupados por turno.
 //Imprimir los gastos en sueldos de cada turno.
 
