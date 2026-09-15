ad700fd8c67428a48378e4c9e8c5f65e01cf8ea1
kiara2112 <kiarafalsa123@gmail.com>
2026-09-14 22:54:25 -0300

    17 - Funciones con parámetros de tipo int, float y char
    Desarrollar una función que reciba como parámetro un caracter. Si llega una 'h' mostrar por pantalla el mensaje "hombre", si llega una 'm' mostrar el mensaje "mujer".
    Llamar desde la función main pasando una vez una 'h' y otra vez una 'm'.

--- Funciones/DATOS/Code92.c
+++ Funciones/DATOS/Code92.c
@@ -1,5 +1,5 @@
-//Desarrollar una funciÛn que reciba como par·metro un caracter. Si llega una 'h' mostrar por pantalla el mensaje "hombre",
-//si llega una 'm' mostrar el mensaje "mujer". Llamar desde la funciÛn main pasando una vez una 'h' y otra vez una 'm'.
+//Desarrollar una funci√≥n que reciba como par√°metro un caracter. Si llega una 'h' mostrar por pantalla el mensaje "hombre",
+//si llega una 'm' mostrar el mensaje "mujer". Llamar desde la funci√≥n main pasando una vez una 'h' y otra vez una 'm'.
 
 #include<stdio.h>
 #include<conio.h>
