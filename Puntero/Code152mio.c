f42496b6e307d490b62a1917c0f423a7e8e96f1d
kiara2112 <kiarafalsa123@gmail.com>
2026-09-15 00:05:01 -0300

    33 - Parámetros de una función de tipo punteros a int, float y char
    Se tiene la siguiente declaración de registro:
    Definir tres variables de tipo país en la función main.
    Crear una función que reciba un puntero de tipo pais y cargue por teclado el nombre del país y la cantidad de habitantes.
    Mostrar en otra función los datos cargados de cada país.

--- Puntero/Code152mio.c
+++ Puntero/Code152mio.c
@@ -1,7 +1,7 @@
-//Se tiene la siguiente declaración de registro:
-//Definir tres variables de tipo país en la función main.
-//Crear una función que reciba un puntero de tipo pais y cargue por teclado el nombre del país y la cantidad de habitantes.
-//Mostrar en otra función los datos cargados de cada país.
+//Se tiene la siguiente declaraciÃ³n de registro:
+//Definir tres variables de tipo paÃ­s en la funciÃ³n main.
+//Crear una funciÃ³n que reciba un puntero de tipo pais y cargue por teclado el nombre del paÃ­s y la cantidad de habitantes.
+//Mostrar en otra funciÃ³n los datos cargados de cada paÃ­s.
 
 #include<stdio.h>
 #include<conio.h>
@@ -20,7 +20,7 @@ void cargar(struct pais *ppais)
         scanf("%i", &ppais->cantidadhab); //aca debemos de poner & porque es un numero (no es gets) y debemos decir que mandamos esto y el %i (ES IGUAL QUE NORMALMENTE, solo con otra declaracion despues donde va
 }
 
-void imprimir(struct pais ppais)//aca como no cambiamos los datos, va sin * adelante de ppais (ACA POR QUÉ NO VA * Y EN 154 SI)
+void imprimir(struct pais ppais)//aca como no cambiamos los datos, va sin * adelante de ppais (ACA POR QUÃ‰ NO VA * Y EN 154 SI)
 {
     printf("los habitantes de %s son %i\n", ppais.nombre, ppais.cantidadhab);
 }
