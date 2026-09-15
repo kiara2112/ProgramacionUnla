fe9bc4b35e72f6bebacbf29aa9f0bd3cedc73641
kiara2112 <kiarafalsa123@gmail.com>
2026-09-15 00:17:31 -0300

    48 - Recursividad: Conceptos básicos
    Otro problema típico que se presenta para analizar la recursividad es el obtener el factorial de un número.
    Recordar que el factorial de un número es el resultado que se obtiene de multiplicar dicho número por el anterior y así sucesivamente hasta llegar a uno.
    Ej. el factorial de 4 es 4 * 3 * 2 * 1 es decir 24.

--- Recursividad/Code177.c
+++ Recursividad/Code177.c
@@ -1,5 +1,5 @@
-//Otro problema típico que se presenta para analizar la recursividad es el obtener el factorial de un número.
-//Recordar que el factorial de un número es el resultado que se obtiene de multiplicar dicho número por el anterior y así sucesivamente hasta llegar a uno.
+//Otro problema tÃ­pico que se presenta para analizar la recursividad es el obtener el factorial de un nÃºmero.
+//Recordar que el factorial de un nÃºmero es el resultado que se obtiene de multiplicar dicho nÃºmero por el anterior y asÃ­ sucesivamente hasta llegar a uno.
 //Ej. el factorial de 4 es 4 * 3 * 2 * 1 es decir 24.
 
 #include <stdio.h>
