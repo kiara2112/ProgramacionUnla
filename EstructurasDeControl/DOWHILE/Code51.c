6545ad21dbfa5255465ec66f6fcec59ece55fd0b
kiara2112 <kiarafalsa123@gmail.com>
2026-09-14 21:23:54 -0300

    11 - Estructura repetitiva do while
    Escribir un programa que solicite la carga de un número entre 0 y 999, y nos muestre un mensaje de cuántos dígitos tiene el mismo. Finalizar el programa cuando se cargue el valor 0.

--- EstructurasDeControl/DOWHILE/Code51.c
+++ EstructurasDeControl/DOWHILE/Code51.c
@@ -1,4 +1,4 @@
-//Escribir un programa que solicite la carga de un número entre 0 y 999, y nos muestre un mensaje de cuántos dígitos tiene el mismo. Finalizar el programa cuando se cargue el valor 0.
+//Escribir un programa que solicite la carga de un nÃºmero entre 0 y 999, y nos muestre un mensaje de cuÃ¡ntos dÃ­gitos tiene el mismo. Finalizar el programa cuando se cargue el valor 0.
 
 #include<stdio.h>
 #include<conio.h>
