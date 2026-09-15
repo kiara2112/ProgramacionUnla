e556bc9392b06cf67e6ac0eb3b6b0ea65dee35f5
kiara2112 <kiarafalsa123@gmail.com>
2026-09-14 21:36:39 -0300

    11 - Estructura repetitiva do while
    En un banco se procesan datos de las cuentas corrientes de sus clientes. De cada cuenta corriente se conoce: número de cuenta y saldo actual. El ingreso de datos debe finalizar al ingresar un valor negativo en el número de cuenta.
    Se pide confeccionar un programa que lea los datos de las cuentas corrientes e informe:
    a)De cada cuenta: número de cuenta y estado de la cuenta según su saldo, sabiendo que:
    Estado de la cuenta	'Acreedor' si el saldo es >0.
    			'Deudor' si el saldo es <0.
    			'Nulo' si el saldo es =0.
    b) La suma total de los saldos acreedores.

--- EstructurasDeControl/DOWHILE/Code55mio.c
+++ EstructurasDeControl/DOWHILE/Code55mio.c
@@ -1,7 +1,7 @@
-//En un banco se procesan datos de las cuentas corrientes de sus clientes. De cada cuenta corriente se conoce: número de cuenta y saldo actual.
-//El ingreso de datos debe finalizar al ingresar un valor negativo en el número de cuenta.
+//En un banco se procesan datos de las cuentas corrientes de sus clientes. De cada cuenta corriente se conoce: nÃºmero de cuenta y saldo actual.
+//El ingreso de datos debe finalizar al ingresar un valor negativo en el nÃºmero de cuenta.
 //Se pide confeccionar un programa que lea los datos de las cuentas corrientes e informe:
-//a)De cada cuenta: número de cuenta y estado de la cuenta según su saldo, sabiendo que:
+//a)De cada cuenta: nÃºmero de cuenta y estado de la cuenta segÃºn su saldo, sabiendo que:
 //Estado de la cuenta	'Acreedor' si el saldo es >0.
 //			'Deudor' si el saldo es <0.
 //			'Nulo' si el saldo es =0.
