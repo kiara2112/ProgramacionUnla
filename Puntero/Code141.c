888f4b1ad9896671e939708ad2e0b5ff9f7af2a6
kiara2112 <kiarafalsa123@gmail.com>
2026-09-14 23:44:53 -0300

    32 - Variables de tipo puntero
    Definir dos variables enteras y almacenar valores por asignación. Definir una variable puntero a entero y guardar sucesivamente las direcciones de dichas dos variables y acceder a sus valores.

--- Puntero/Code141.c
+++ Puntero/Code141.c
@@ -1,9 +1,9 @@
-//Definir dos variables enteras y almacenar valores por asignación. Definir una variable puntero a entero
+//Definir dos variables enteras y almacenar valores por asignaciÃ³n. Definir una variable puntero a entero
 //y guardar sucesivamente las direcciones de dichas dos variables y acceder a sus valores.
 
 //Para que un programa sea muy eficiente.
 //Modificar variables de tipo int, float, struct etc. en otras funciones.
-//Poder requerir y liberar memoria durante la ejecución del programa (hay muchas situaciones donde no sabemos cuanto espacio reservar)
+//Poder requerir y liberar memoria durante la ejecuciÃ³n del programa (hay muchas situaciones donde no sabemos cuanto espacio reservar)
 
 #include<stdio.h>
 #include<conio.h>
@@ -13,7 +13,7 @@ int main()
     int valor1=10;
     int valor2=20;
     int *pe; //asteristico es que la variable es tipo puntero (puntero de variables de tipos enteros)
-    pe=&valor1; //pe(variable que apunta a una dirección de memoria donde se almacena un entero.) valor1(variable entera) es incorrecto (pe=valor1) porque no son lo mismo
+    pe=&valor1; //pe(variable que apunta a una direcciÃ³n de memoria donde se almacena un entero.) valor1(variable entera) es incorrecto (pe=valor1) porque no son lo mismo
     printf("Lo apuntado por pe es:%i\n",*pe); //*pe imprime lo apuntado por el puntero pe (en este caso 10)
     printf("La direccion que almacena pe es:%p\n",pe); //%p imprime el puntero
     pe=&valor2; //cambiamos al valor 2
