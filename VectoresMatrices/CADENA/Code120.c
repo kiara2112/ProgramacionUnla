b2cd64342a8a1a4a85a0a0c1c4e8ab26f4a20f5d
kiara2112 <kiarafalsa123@gmail.com>
2026-09-14 23:21:24 -0300

    23 - Estructura de datos tipo matriz (elementos char)
    Confeccionar un programa que permita ingresar en una matriz de tipo char los nombres de artículos para la venta. Hacer luego una función que imprima los nombres de dichos artículos.

--- VectoresMatrices/CADENA/Code120.c
+++ VectoresMatrices/CADENA/Code120.c
@@ -1,4 +1,4 @@
-//Confeccionar un programa que permita ingresar en una matriz de tipo char los nombres de artículos para la venta. Hacer luego una función que imprima los nombres de dichos artículos.
+//Confeccionar un programa que permita ingresar en una matriz de tipo char los nombres de artÃ­culos para la venta. Hacer luego una funciÃ³n que imprima los nombres de dichos artÃ­culos.
 
 #include<stdio.h>
 #include<conio.h>
